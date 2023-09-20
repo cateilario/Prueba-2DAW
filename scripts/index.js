5ab13aba4c8b01450725bf5acbe7d5d08411368b
unknown <caterinailariopaz@alumnos.tuniversformacion.es>
2023-09-20 11:35:46 +0200

    Modificaciones

--- scripts/index.js
+++ scripts/index.js
@@ -1,9 +1,15 @@
-let boton=document.querySelector("#color");
-boton.addEventListener("click", ()=>
-    {
-        let aleatorio1= Math.floor(Math.random() * (255+1-0) + 0);
-        let aleatorio2= Math.floor(Math.random() * (255+1-0) + 0);
-        let aleatorio3= Math.floor(Math.random() * (255+1-0) + 0);
-        let aleatorio4= Math.floor(Math.random() * (255+1-0) + 0);
-        document.body.style.backgroundColor=`rgba(${aleatorio1}, ${aleatorio2}, ${aleatorio3}, ${aleatorio4})`;
-    });
+//Funcion para generar un color aleatorio en formato hexadecimal
+    const generateRandomColor = () => {
+        const color ='#' + Math.floor(Math.random()* 16777215).toString(16);
+        console.log("Color hexadecimal generado: " + color)
+        return color
+    }
+
+//Funcion para cambiar el color fondo del body
+    const changeBackgroundColor = () => {
+        const newColor = generateRandomColor();
+        document.body.style.backgroundColor = newColor;
+    }
+
+//Agregar un evento al botón para cambiar color al hacer click
+    const changeColorBtn = document.getElementById("changeColorBtn");
